cca0d51905e6e74ce81f0191927773cd92af4add
Pallavikothawade <Pallavijkothawade@gmail.com>
2022-12-08 19:16:07 +0530

    Welcome the csharp

---
+++ Typesofvariables/Localvariables.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Typesofvariables
+{
+    internal class Localvariables
+    {
+        public static void StudentAge()
+        {
+            int age = 0;
+            age = age + 10;
+            Console.WriteLine("Student age is:"+age);
+
+        }
+    }
+}
